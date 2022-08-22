7a254e97bb1c7e7119aa9cd72ffbd0b1c43e1b31
Vasu_Devan_S <vasu.devan.skj@gmail.com>
2022-08-22 23:17:14 +0530

    V6 Day6
    Creating Server using of Flask

---
+++ BHP/server/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request,jsonify
+app = Flask(__name__)
+
+@app.route("/get_location_names")
+def get_location_names():
+    response = jsonify({
+        'locations' : util.get_location_names()
+    })
+    response.headers.add('Acess-Control-Allow Origin','*')
+    return response
+if __name__=="__main__":
+    print("Syarting Python Flask Server For House Price Prediction...")
+    app.run()
